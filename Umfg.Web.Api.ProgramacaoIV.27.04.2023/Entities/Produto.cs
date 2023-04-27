ff88cbccaaec1ff788c46e08bdb9bd266513a786
Felipe <flipeoliveira007@gmail.com>
2023-04-27 20:49:35 -0300

    first commit

---
+++ Umfg.Web.Api.ProgramacaoIV.27.04.2023/Entities/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Umfg.Web.Api.ProgramacaoIV._27._04._2023.Entities
+{
+    public sealed class Produto
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; private set;}
+
+        [JsonPropertyName("codigoBarra")]
+        public string CodigoBarra { get; set;}
+
+        [JsonPropertyName("descricao")]
+        public string Descricao { get; set;}
+
+        [JsonPropertyName("valor")]
+        public decimal Valor { get; set;}
+
+        public Produto() 
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
